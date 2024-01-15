736f002c0da1affa03b60274b55d5cb4d56069f9
Osvaldo Aurélio <osvaldo.ribeiro@lambda3.com.br>
2024-01-15 12:20:37 -0300

    add remove-html project

---
+++ projects/remove-html-front-end/script.js
@@ -0,0 +1,72 @@
+const apiUrl = 'https://remove-html.onrender.com/' || 'http://localhost:3000/';
+
+const textLabel = 'Clique para selecionar ou arraste um arquivo de texto para processar';
+
+const fileLabel = document.getElementById('file-label');
+const fileInput = document.getElementById('file-input');
+const btnProcess = document.getElementById('btn-process');
+
+fileLabel.textContent = textLabel;
+
+const emptyFileInput = () => {
+  fileInput.value = '';
+  fileLabel.textContent = textLabel;
+  btnProcess.classList.remove('active');
+};
+
+const fillFIleInput = name => {
+  fileLabel.textContent = name;
+  btnProcess.classList.add('active');
+};
+
+const createDownloadLink = (blob, fileName) => {
+  const downloadUrl = URL.createObjectURL(blob);
+
+  const downloadLink = document.createElement('a');
+
+  downloadLink.href = downloadUrl;
+  downloadLink.download = fileName
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  
+  return { downloadUrl, downloadLink };
+}
+
+const removeDownloadLink = (downloadUrl, downloadLink) => {  
+  URL.revokeObjectURL(downloadUrl);
+
+  document.body.removeChild(downloadLink);
+}
+
+fileInput?.addEventListener('change', ({ target }) => {
+  const [file] = target.files;
+
+  if (!file || !file.type.includes('text')) return emptyFileInput();
+
+  fillFIleInput(file.name);
+});
+
+btnProcess?.addEventListener('click', async () => {
+  const [file] = fileInput.files;
+
+  if (!file) return alert('Nenhum arquivo para processar\n\nUse a área acima e selecione um arquivo de texto para processar!');
+
+  const formData = new FormData();
+  formData.append('file', file);
+
+  const response = await fetch(`${apiUrl}process`, {
+    method: 'POST',
+    body: formData,
+  });
+
+  emptyFileInput();
+
+  if (!response.ok) return console.error('Erro ao processar o arquivo');
+  
+  const fileName = response.headers.get('X-Filename') ?? 'no-html.txt';
+  const blob = await response.blob();
+  
+  const { downloadUrl, downloadLink } = createDownloadLink(blob, fileName);
+
+  removeDownloadLink(downloadUrl, downloadLink);
+});
